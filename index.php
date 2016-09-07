0bb7c2b3481551b56985d25f0d4407219b14c9d6
tirolbot <heigher@gmail.com>
2016-09-07 18:58:51 +0900

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-require_once __DIR__.'vendor/autoload.php';
+require_once __DIR__.'/vendor/autoload.php';
 use Symfony\Component\HttpFoundation\Request;
 date_default_timezone_set('Asia/Tokyo');
 echo 'hello, world';
