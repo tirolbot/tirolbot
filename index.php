031dd36c15e4243a314266c38ccf6e9e498b892d
tirolbot <heigher@gmail.com>
2017-03-07 16:17:11 +0900

    【参考】 http://tatsuya1970.com/?p=8115

--- index.php
+++ index.php
@@ -1,35 +1,26 @@
-<?php
-
-$accessToken = 'IosqU5IsTZm7bu21Kbi5bt2F0MaX42lcshSpCkGnY6lUtw6vAW/Q7Dq7GvOBMKyCxXCcQ4KGKB1rWae1FP6dZAKw4ecSIzD9QI1FRQZ+0jOlN6mRaYHzuEr/BqbcdvP0MuWfXgQDOd03QiXK3FdSngdB04t89/1O/w1cDnyilFU=';
+$accessToken = ‘IosqU5IsTZm7bu21Kbi5bt2F0MaX42lcshSpCkGnY6lUtw6vAW/Q7Dq7GvOBMKyCxXCcQ4KGKB1rWae1FP6dZAKw4ecSIzD9QI1FRQZ+0jOlN6mRaYHzuEr/BqbcdvP0MuWfXgQDOd03QiXK3FdSngdB04t89/1O/w1cDnyilFU=’;
 
 //ユーザーからのメッセージ取得
 $json_string = file_get_contents('php://input');
 $jsonObj = json_decode($json_string);
 
-$type = $jsonObj->{"events"}[0]->{"message"}->{"type"};
-//メッセージ取得
+$type = $jsonObj--->{"events"}[0]->{"message"}->{"type"};
 $text = $jsonObj->{"events"}[0]->{"message"}->{"text"};
-//ReplyToken取得
 $replyToken = $jsonObj->{"events"}[0]->{"replyToken"};
 
-//メッセージ以外のときは何も返さず終了
-if($type != "text"){
-    exit;
-}
 
-//docomo返信
+//ドコモの雑談データ取得
 $response = chat($text);
 
-//返信データ作成
 $response_format_text = [
     "type" => "text",
-    "text" => $response
-    ];
+    "text" =>  $response
+  ];
 
 $post_data = [
-    "replyToken" => $replyToken,
-    "messages" => [$response_format_text]
-    ];
+	"replyToken" => $replyToken,
+	"messages" => [$response_format_text]
+	];
 
 $ch = curl_init("https://api.line.me/v2/bot/message/reply");
 curl_setopt($ch, CURLOPT_POST, true);
@@ -44,14 +35,13 @@ $result = curl_exec($ch);
 curl_close($ch);
 
 
-
 //ドコモの雑談APIから雑談データを取得
 function chat($text) {
     // docomo chatAPI
-    $api_key = '7a7949696d525668572f55355251503774314e2f71664e4b474458632f6447554f6c785264385055324641';
+    $api_key = ‘7a7949696d525668572f55355251503774314e2f71664e4b474458632f6447554f6c785264385055324641’;
     $api_url = sprintf('https://api.apigw.smt.docomo.ne.jp/dialogue/v1/dialogue?APIKEY=%s', $api_key);
     $req_body = array('utt' => $text);
-
+    
     $headers = array(
         'Content-Type: application/json; charset=UTF-8',
     );
